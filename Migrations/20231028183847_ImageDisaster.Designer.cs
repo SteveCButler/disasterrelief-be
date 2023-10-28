4b1c0bbed68264da644e24182f697ab67fd036f6
Demansell <demansell2016@gmail.com>
2023-10-28 13:39:38 -0500

    Added Image to Disaster

--- Migrations/20231028183847_ImageDisaster.Designer.cs
+++ Migrations/20231028183847_ImageDisaster.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -10,9 +11,10 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace disasterrelief_be.Migrations
 {
     [DbContext(typeof(DisasterReliefDbContext))]
-    partial class DisasterReliefDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231028183847_ImageDisaster")]
+    partial class ImageDisaster
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
