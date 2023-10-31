e274b4566a6f138628b6bc712ca9b796d554ef3a
Steve Butler <butlersteve.c@gmail.com>
2023-10-30 20:25:51 -0500

    fix categoryId type in Item model, add disaster.Image to update disaster endpoint

---
+++ Migrations/20231030232742_categoryIdToInt.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace disasterrelief_be.Migrations
+{
+    public partial class categoryIdToInt : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
